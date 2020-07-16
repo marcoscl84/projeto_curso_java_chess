3d78fc6d346cada1b0f2ef20d2d33b2e0e5a4720
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2020-07-16 20:26:12 -0300

    Possible moves of a piece

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-public class Piece {
+public abstract class Piece {
 
 	protected Position position;
 	private Board board;
@@ -12,5 +12,29 @@ public class Piece {
 
 	protected Board getBoard() {
 		return board;
+	}
+	
+	public abstract boolean[][] possibleMoves();
+	//operação genérica de possibilidades de movimentos da peça
+	
+	public boolean possibleMove(Position position) {
+		/* Método concreto utilizando método abstrato. Método faz um gancho com 
+		 * com a subclasse.
+		 */
+		return possibleMoves()[position.getRow()][position.getColumn()];
+		//chama uma possível implementação de alguma subclasse concreta da classe piece
+	}
+	
+	public boolean isThereAnyPossibleMove() {
+		//método varre a matriz para ver se há alguma posição da matriz verdadeira para a peça mover.
+		boolean[][] mat = possibleMoves();
+		for (int i=0; i<mat.length; i++) {
+			for (int j=0; j<mat.length; j++) {
+				if (mat[i][j]) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
